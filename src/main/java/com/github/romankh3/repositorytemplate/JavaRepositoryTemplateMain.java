66febba8c914cb2895901e32622603ab05ed31c9
Roman Beskrovnyi <roman.beskrovnyy@gmail.com>
2019-12-19 15:33:26 +0200

    added src folder (#2)

---
+++ src/main/java/com/github/romankh3/repositorytemplate/JavaRepositoryTemplateMain.java
@@ -0,0 +1,13 @@
+package com.github.romankh3.repositorytemplate;
+
+/**
+ * Main class for Java Repository Template.
+ */
+public class JavaRepositoryTemplateMain {
+
+    public static final String JAVA_REPOSITORY_TEMPLATE = "java-repository-template";
+
+    public static void main(String[] args) {
+        System.out.println(String.format("Hello world, %s!",JAVA_REPOSITORY_TEMPLATE));
+    }
+}
